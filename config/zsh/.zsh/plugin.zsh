ba1c65c134e7da3d6fee9fecaf8100d210439262
Andreev Dmitry <dimitrius.nedraw@gmail.com>
2025-02-13 20:06:40 +0300

    ZSH: Add fzf plugin

--- config/zsh/.zsh/plugin.zsh
+++ config/zsh/.zsh/plugin.zsh
@@ -1,2 +1,3 @@
 source $ZSH_DIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $ZSH_DIR/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $ZSH_DIR/plugins/zsh-fzf-history-search/zsh-fzf-history-search.zsh
