8b471cc0fb1c7a11d81800cb35115f70943668c3
Dmitry Andreev <dimitrius.nedraw@gmail.com>
2025-02-24 18:22:16 +0300

    fix plugin completion

--- stow/zsh/.config/zsh/plugin.zsh
+++ stow/zsh/.config/zsh/plugin.zsh
@@ -1,4 +1,3 @@
-fpath=($ZSH_DIR/plugins/zsh-completions/src $fpath)
 source $ZSH_DIR/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSH_DIR/plugins/zsh-fzf-history-search/zsh-fzf-history-search.zsh
 source $ZSH_DIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- stow/zsh/.zshrc
+++ stow/zsh/.zshrc
@@ -1,6 +1,6 @@
 setopt autocd
-
 ZSH_DIR="${funcsourcetrace[1]%/*}/.config/zsh"
+fpath=($ZSH_DIR/plugins/zsh-completions/src $fpath)
 source $ZSH_DIR/history.zsh
 source $ZSH_DIR/comp.zsh
 source $ZSH_DIR/prompt.zsh
